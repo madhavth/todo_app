662b3ba0eb86c683c6c9f10fd4d9854b10a46139
madhavth <madhavth77@gmail.com>
2022-11-18 13:43:57 -0600

    add todo functionality added

--- src/Component/AddTodo/AddTodo.js
+++ src/Component/AddTodo/AddTodo.js
@@ -1,11 +1,29 @@
 import './AddTodo.css';
 import {useRef} from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 export default function AddTodo() {
     const formRef = useRef();
-
+    const navigate = useNavigate();
+    
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const todo = {
+            title: formRef.current.title.value,
+            description: formRef.current.description.value,
+            done: formRef.current.done.value === "true"
+        };
+        
+        axios.post('http://localhost:8080/todos', todo)
+            .then((response) => {
+                navigate('/');
+            })
+            .catch((e) => {
+                console.log(e.message);
+            });
+
     };
 
     return (<div>
@@ -17,6 +35,6 @@ export default function AddTodo() {
             <label>Done</label>
             <input name={'done'} type={'checkbox'}/>
         </form>
-        <button onSubmit={onSubmit}>Submit</button>
+        <button onClick={onSubmit}>Submit</button>
     </div>);
 }
--- src/Container/TodoList/TodoList.js
+++ src/Container/TodoList/TodoList.js
@@ -2,17 +2,29 @@ import Todo from "../../Component/Todo/Todo";
 import './TodoList.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {CircularProgress} from "@mui/material";
 
 export default function TodoList(props) {
-    const [todos, setTodos] = useState([]);
+    const [todosState, setTodosState] = useState({
+        'state': 'fetching',
+        'data': [],
+        message: ''
+    });
 
     const fetchTodos = () => {
         axios.get('http://localhost:8080/todos')
             .then((response) => {
-                setTodos(response.data);
+                setTodosState({
+                    'state': 'done',
+                    'data': response.data
+                });
             })
             .catch((error) => {
                 console.log(error.message);
+                setTodosState({
+                    'state': 'error',
+                    'message': `${error.message}`
+                })
             });
     };
 
@@ -20,9 +32,34 @@ export default function TodoList(props) {
         fetchTodos();
     }, []);
 
+    if (todosState.state === 'fetching') {
+        return <CircularProgress/>;
+    }
+    
+    if(todosState.state === 'error') {
+        return <div>
+            <div><text>{todosState.message}</text></div>
+            <button>retry</button>
+        </div>;
 
-    return todos.map((todo) => {
+    }
+
+    if (todosState.data.length === 0) {
+        return <NoTodosAddedYet/>
+    }
+
+    const todosList = todosState.data.map((todo) => {
         return <Todo todo={todo}/>
     });
+    
+    return (<div className={'todoList'}>
+        {todosList}
+    </div>);
 
+}
+
+function NoTodosAddedYet() {
+    return (<div className={'noTodo'}>
+        No todos added yet!!!
+    </div>);
 }
